82449f088d215dd94a5b2deb94b43a86905ca971
Charles Chamberlain <charlespipin@gmail.com>
2018-09-16 18:41:25 -0400

    Rename app to apply

--- src/reason-parser/reason_pprint_ast.ml
+++ src/reason-parser/reason_pprint_ast.ml
@@ -3409,7 +3409,7 @@ let printer = object(self:'self)
     match x.pexp_desc with
     | Pexp_apply (
         {pexp_desc = Pexp_ident {txt = Lident "raise"; _}},
-        [(_, { pexp_desc = Pexp_construct ({txt = Ldot (lident, "App"); _},
+        [(_, { pexp_desc = Pexp_construct ({txt = Ldot (lident, "Apply"); _},
               Some {pexp_desc = Pexp_tuple [_ ;
                 {pexp_desc =
                   Pexp_constant (Pconst_string (source, _)); _}]; _} )})]) ->
